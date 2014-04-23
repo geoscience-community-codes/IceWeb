3cb5623dd6d1da9fe75d8e30ec3797bf243bd95a
Glenn Thompson <glennthompson1971@gmail.com>
2014-04-22 20:31:55 -0400

    Added sam.m

--- startup.m
+++ startup.m
@@ -1,1 +1,11 @@
-startup_tremor
+startup_universal
+%warning off;
+%matlab_antelope=getenv('MATLAB_ANTELOPE');
+%addpath(genpath(matlab_antelope));
+addpath(genpath('GISMO'));
+addpath(genpath('matlab'));
+%javaaddpath('lib/swarm.jar');
+%javaaddpath('lib/usgs.jar');
+%javaaddpath('lib/swarm-bin.jar');
+
+
